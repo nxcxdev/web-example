400902a81e321dcf2c85ebbc213eb22f749467f6
nxcxdev <am.nico.vo@gmail.com>
2023-03-28 12:44:46 +0200

    fix(footer): fix footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -16,81 +16,80 @@ export default function Footer() {
                     </p>
                 </div>
                 <div className="footer-links-container">
-                        <div className="footer-container">
-                            <div className="container-heading">Wallapop</div>
-                            <ul className="link-list">
-                                <li>
-                                    <a href="" className="footer-link">
+                    <div className="footer-container">
+                        <div className="container-heading">Wallapop</div>
+                        <ul className="link-list">
+                            <li>
+                                <a href="" className="footer-link">
                                     Quiénes somos
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="" className="footer-link">
-                                    Cómo funciona                                    
-                                    </a>
-                                </li>
-                                    <li>
-                                    <a href="" className="footer-link">
-                                    Brand Book                                    
-                                    </a>
-                                    </li>
-                                    <li>
-                                    <a href="" className="footer-link">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" className="footer-link">
+                                    Cómo funciona
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" className="footer-link">
+                                    Brand Book
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" className="footer-link">
                                     Prensa
-                                    </a>
-                                    </li>
-                                    <li>
-                                    <a href="" className="footer-link">
-                                        Empleo
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>                      
-                        <div className="footer-container">
-                            <div className="container-heading">Soporte</div>
-                            <ul className="link-list">
-                                <li>
-                                    <a href="" className="footer-link">
-                                        Centro de ayuda
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="" className="footer-link">
-                                        Reglas de publicación
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="" className="footer-link">
-                                        Consejos de seguridad
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="footer-container">
-                            <div className="container-heading">Legal</div>
-                            <ul className="link-list">
-                                <li>
-                                    <a href="" className="footer-link">
-                                        Aviso legal
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="" className="footer-link">
-                                        Condiciones de uso
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="" className="footer-link">
-                                        Política de privacidad
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="" className="footer-link">
-                                        Cookies
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" className="footer-link">
+                                    Empleo
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="footer-container">
+                        <div className="container-heading">Soporte</div>
+                        <ul className="link-list">
+                            <li>
+                                <a href="" className="footer-link">
+                                    Centro de ayuda
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" className="footer-link">
+                                    Reglas de publicación
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" className="footer-link">
+                                    Consejos de seguridad
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="footer-container">
+                        <div className="container-heading">Legal</div>
+                        <ul className="link-list">
+                            <li>
+                                <a href="" className="footer-link">
+                                    Aviso legal
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" className="footer-link">
+                                    Condiciones de uso
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" className="footer-link">
+                                    Política de privacidad
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" className="footer-link">
+                                    Cookies
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
